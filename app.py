c1cd7cb30cd6ae96d9ff98df493edd4001f12348
Miranda Prasad <miranda_prasad@intuit.com>
2022-03-10 02:05:44 +0000

    changes made

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 
        'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
-mycolumn='corn'
+mycolumn='fruits fresh'
 myheading1 = f"Wow! That's a lot of {mycolumn}!"
-mygraphtitle = '2011 US Agriculture Exports by State'
+mygraphtitle = 'Interactive 2011 US Agriculture Exports by State'
 mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
 mycolorbartitle = "Millions USD"
 tabtitle = 'Old McDonald'
@@ -26,21 +26,6 @@ githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
 import pandas as pd
 df = pd.read_csv('assets/usa-2011-agriculture.csv')
 
-fig = go.Figure(data=go.Choropleth(
-    locations=df['code'], # Spatial coordinates
-    z = df[mycolumn].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = mycolorscale,
-    colorbar_title = mycolorbartitle,
-))
-
-fig.update_layout(
-    title_text = mygraphtitle,
-    geo_scope='usa',
-    width=1200,
-    height=800
-)
-
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -51,16 +36,55 @@ app.title=tabtitle
 
 app.layout = html.Div(children=[
     html.H1(myheading1),
-    dcc.Graph(
-        id='figure-1',
-        figure=fig
-    ),
+    html.Div([
+        html.Div([
+                html.H6('Select a variable of your choosing:'),
+                dcc.Dropdown(
+                    id='options-drop',
+                    options=[{'label': i, 'value': i} for i in list_of_columns],
+                    value='fruits fresh'
+                ), 
+        ], className='two columns'),
+        html.Div([dcc.Graph( id='figure-1'),
+                 ], className='ten columns'),
+    ], className='twelve columns'),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
     ]
 )
+        
+        
+########## Define Callback
+@app.callback(Output('figure-1', 'figure'),
+              [Input('options-drop', 'value')])
+def make_figure(mycolumn):
+    mygraphtitle= f'Exports of {mycolumn} in 2011'
+    mycolorscare= 'ylorrd'  # Note: The error message will list possible color scales.
+    mycolorbartitle= 'Millions USD'
+    
+    fig = go.Figure(data=go.Choropleth(
+        locations=df['code'], # Spatial coordinates
+        z = df[mycolumn].astype(float), # Data to be color-coded
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        colorscale = mycolorscale,
+        colorbar_title = mycolorbartitle,
+    ))
+
+    fig.update_layout(
+        title_text = mygraphtitle,
+        geo_scope='usa',
+        width=1200,
+        height=800
+    )
+
+    
+    
+    return fig       
 
 ############ Deploy
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
+
+    
+    
